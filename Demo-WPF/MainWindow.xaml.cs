7286dc0905869e8b431e814a5d00e201ecb80fe1
Philippe Berthonneau <philippe.berthonneau@dxc.com>
2020-12-15 18:54:21 +0100

    app alone

---
+++ Demo-WPF/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Demo_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Click");
+        }
+
+        private void Label_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Enter");
+        }
+
+        private void Label_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Leave");
+        }
+    }
+}
